9aaf4d36143726a96129bfe98164f34ed5f7b786
guzilla(NALA) <13578427+gusvalenzuela@users.noreply.github.com>
2020-11-27 13:45:26 -0800

    Convert FeelingLucky from json to js module

---
+++ src/utils/FeelingLucky.js
@@ -0,0 +1,22 @@
+export const version = 0;
+export const name = "Foods in Feeling Lucky Array";
+export const foods = [
+  "Pizza",
+  "Burger",
+  "Wine",
+  "Chinese",
+  "Beef",
+  "Chicken",
+  "Pork",
+  "Tacos",
+  "Kale",
+  "Thai",
+  "Vietnamese",
+  "American",
+  "Orange Chicken",
+  "Orange Juice",
+  "Coconut",
+  "Acai",
+  "Corn",
+  "Hot Dog",
+];
